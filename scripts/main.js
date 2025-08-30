0a3a81ae1d3b39a229054f16d069c2000cb69721
AlbertoRoca-web <alroca308@gmail.com>
2025-08-30 12:21:58 -0400

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Toggle Hamburger Menu visibility
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});

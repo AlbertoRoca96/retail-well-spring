cbc73df1a93f822c646b2ae451c52470183fff41
AlbertoRoca-web <alroca308@gmail.com>
2025-08-30 12:45:53 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-document.getElementById('mobile-menu').addEventListener('click', function () {
-    document.querySelector('.navbar').classList.toggle('active');
+// Get the menu icon and navigation bar
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('nav');
+
+// Add event listener for clicking the hamburger menu
+menuIcon.addEventListener('click', () => {
+    // Toggle the 'active' class to show or hide the menu
+    nav.classList.toggle('active');
 });
